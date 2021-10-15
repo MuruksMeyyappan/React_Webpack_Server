9c372194f0cd1b6747c42d3cc4e655ce75235d12
MuruksMeyyappan <muruksmroger@gmail.com>
2021-10-15 12:53:57 +0530

    Stipe Payment

---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require('express')
+
+const app = express()
+const cors = require('cors')
+const Stripe = require('stripe')
+
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/pay',async (req,res) => {
+    try {
+        const amount = 2000;
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: "inr",
+            payment_method_types: ["card"],
+            metadata: {
+              name: "value",
+            },
+          });
+          const clientSecret = paymentIntent.client_secret;
+          res.json({ clientSecret, message: "Payment Initiated" });
+    } 
+    catch(err){
+        console.error(err)
+        res.send(500).json({message: 'Internal server error'})
+    }
+})
+
+
+app.post('/stripe',(req,res) => {
+    if(req.body.type === 'payment_intent.created'){
+        console.log(`${req.body.data.object.metadata.name} initiated payment`);
+    }
+
+    if(req.body.type === 'payment_intent.succeeded'){
+        console.log(`${req.body.data.object.metadata.name} succeeded payment`);
+    }
+})
+
+app.listen(5000,()=> console.log("server is running on Port ::: 5000"))
+ 
